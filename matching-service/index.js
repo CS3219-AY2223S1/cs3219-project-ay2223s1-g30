b1ee9e66b9e495509747860f18360f6fc290574d
Alfred Koh <alfredkohkj@gmail.com>
2022-09-14 06:41:22 +0800

    Developed: - Leetcode Selection Page - Difficulty Selection - Socket Connection - Socket Emittion - Timer

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from "socket.io";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World from matching-service');
@@ -15,3 +16,19 @@ app.get('/', (req, res) => {
 const httpServer = createServer(app)
 
 httpServer.listen(8001);
+
+const io = new Server(httpServer, {
+    /* socket.io options */
+    cors: {
+        origin: '*',
+      }
+});
+
+io.on("connection", (socket) => {
+    console.log(`Received a connection request with ID: ${socket.id}`)
+    //For testing with Postman
+    socket.on("message", function (data) {
+        console.log("data: " + data);
+    });
+});
+
