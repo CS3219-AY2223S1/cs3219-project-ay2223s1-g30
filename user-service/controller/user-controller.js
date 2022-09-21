6abb5809f193be656085e3b0f3d224f0c2d2008a
Benjykoh <benjaminkoh0909@gmail.com>
2022-09-21 15:36:53 +0800

    Update jwt token TTL

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -262,6 +262,6 @@ export async function updateUser(req, res) {
 // Generate JWT
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: "10d",
+		expiresIn: "1d",
 	});	
 };
